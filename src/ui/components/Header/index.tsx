2bce45422ff264d3bfa2b2025a0527b5507f35d2
yama-chiiii <nagayama0409@gmail.com>
2023-03-06 23:48:38 +0900

    create-header

---
+++ src/ui/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <div className="w-full flex justify-between items-center px-20 h-60 bg-white">
+            <h1 className="text-lg font-black">コン研‐図書館</h1>
+            <button className="text-xs font-black bg-brand-green text-white px-16 py-6 rounded lib-pointer">
+                ログイン
+            </button>
+        </div>
+    );
+};
--- src/ui/pages/Root/index.tsx
+++ src/ui/pages/Root/index.tsx
@@ -1,3 +1,5 @@
+import { Header } from "~/ui/components/Header";
+
 type User = {
     id: string;
 };
@@ -10,7 +12,8 @@ export type Props = {
 export const RootPage = (props: Props) => {
     console.log(props);
     return (
-        <div className="bg-brand-green-light max-w-sm min-h-screen mx-auto px-24 flex flex-col justify-center items-center">
+        <div className="bg-brand-green-light max-w-sm min-h-screen mx-auto flex flex-col  items-center">
+            <Header />
             <h1 className="mb-16  text-5xl font-bold">コン研 - 図書館アプリ</h1>
             <p className="mb-28 text-white text-sm lg:text-lg">XXXX</p>
         </div>
