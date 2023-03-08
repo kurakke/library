790802c1335d45f538554a06cfc4b8f81337ee12
yama-chiiii <nagayama0409@gmail.com>
2023-03-08 23:35:59 +0900

    apply link to header

---
+++ src/features/application/constants/page.ts
@@ -0,0 +1,4 @@
+export const PAGE_PATH = {
+    Root: "/",
+    SignIn: "/signin",
+};
--- src/ui/components/Header/index.tsx
+++ src/ui/components/Header/index.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { PAGE_PATH } from "~/features/application/constants/page";
+
 export type Props = {
     disableCtrls?: boolean;
 };
@@ -5,11 +8,17 @@ export type Props = {
 export const Header = ({ disableCtrls }: Props) => {
     return (
         <div className="w-full flex justify-between items-center px-20 h-60 bg-white">
-            <h1 className="text-lg font-black">コン研‐図書館</h1>
+            <Link href={PAGE_PATH.Root}>
+                <a>
+                    <h1 className="text-lg font-black">コン研‐図書館</h1>
+                </a>
+            </Link>
             {!disableCtrls && (
-                <button className="text-xs font-black bg-brand-green text-white px-16 py-6 rounded lib-pointer">
-                    ログイン
-                </button>
+                <Link href={PAGE_PATH.SignIn}>
+                    <a className="text-xs font-black bg-brand-green text-white px-16 py-6 rounded lib-pointer">
+                        ログイン
+                    </a>
+                </Link>
             )}
         </div>
     );
