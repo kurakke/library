72692eb7a294200569de669e7520ec61dad50adc
yama-chiiii <nagayama0409@gmail.com>
2023-03-30 21:12:57 +0900

    rename profileEdit

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import React from "react";
-import { ProfilePage } from "~/ui/pages/Profile";
+import { ProfilePage } from "~/ui/pages/ProfileEdit";
 const Page: NextPage = () => {
     return <ProfilePage />;
 };
