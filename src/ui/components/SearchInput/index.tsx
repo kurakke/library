484c1af5221b39538cdb21157d396db4e18264f1
yama-chiiii <nagayama0409@gmail.com>
2023-04-02 15:05:24 +0900

    apply search event handler

--- src/ui/components/SearchInput/index.tsx
+++ src/ui/components/SearchInput/index.tsx
@@ -1,18 +1,26 @@
 import SearchIcon from "~assets/svgs/search.svg";
 import Image from "next/image";
+import React, {useRef} from "react";
 
 export const SearchInput = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const handleKeyDown:React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+        if (e.key === 'Enter') {
+            console.log(inputRef.current.value);
+        }
+    }
     return (
         <div　className="flex items-center">
             <div className="flex items-center w-full px-12 h-40 rounded-lg bg-gray-bright">
                 <Image src={SearchIcon}/>
                 <input
-                    type="text"
+                    ref={inputRef}
+                    type="search"
                     name="search"
-                    className="px-12 bg-gray-bright flex-grow"
+                    className="ml-12 bg-gray-bright flex-grow"
+                    onKeyDown={handleKeyDown}
                     placeholder="データベース設計"
                 />
-                <button className="flex justify-center items-center w-52 h-28 rounded bg-brand-green font-black text-xs text-white">保存</button>
             </div>
         </div>
     )
