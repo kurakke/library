9ecff348b16b72c19ddc68090fc53514e77bb7fc
yama-chiiii <nagayama0409@gmail.com>
2023-04-13 00:39:53 +0900

    WIP

--- src/views/pages/Books/index.tsx
+++ src/views/pages/Books/index.tsx
@@ -1,4 +1,4 @@
-import {Header} from "~/ui/components/Header";
+import {useState} from "react";
 import {SearchInput} from "~/ui/components/SearchInput";
 import {DefaultLayout} from "~/ui/layouts/Default";
 import {Book} from "~/ui/components/Book";
@@ -6,6 +6,7 @@ import {PAGE_PATH} from "~/features/application/constants/page";
 import {useRouter} from "next/router";
 import {getServerSideProps} from "~/views/pages/Books/beforeRender";
 import {InferGetServerSidePropsType} from "next";
+import classNames from "classnames";
 
 export const BooksPage = (
     {bookList}: InferGetServerSidePropsType<typeof getServerSideProps>
@@ -19,12 +20,19 @@ export const BooksPage = (
             }
         })
     }
+
+    const [count,setCount] = useState<string>("");
+
     return (
         <div>
             <DefaultLayout>
                 <div className="px-16 mt-16">
                     <SearchInput onSearch={handleSearch}/>
                     <div className="mt-16">
+                        <div className="lib-tab flex justify-around items-center w-full h-40 rounded-lg border-2 border-solid font-black">
+                            <button onClick={() => setCount("全て")} className={classNames({"text-white":count==="全て"})}>全て</button>
+                            <button onClick={() => setCount("貸出可能")}>貸出可能</button>
+                        </div>
                         {
                             bookList.list.map((book) =>
                                 <li key={book.id}><Book book={book}/></li>
