f3a52189ec8080ad3628587fe3120f0573186f75
kurakke <kcanggua@gmail.com>
2023-05-03 01:16:58 +0900

    SignUpページに飛べるようにしました

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,9 @@
+import {NextPage} from "next";
+import React from "react";
+import { SignUpPage } from "~/views/pages/SignUp";
+
+const Page: NextPage = () => {
+    return  <SignUpPage />
+};
+
+export default Page;
