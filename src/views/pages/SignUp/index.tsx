f38594c3c02185c5c9a83ea8b12d7c8688720b00
kurakke <kcanggua@gmail.com>
2023-05-05 01:07:15 +0900

    formに必要な定数を宣言

--- src/views/pages/SignUp/index.tsx
+++ src/views/pages/SignUp/index.tsx
@@ -1,6 +1,12 @@
-
-
+import React from "react";
+import { useState } from "react";
 export const SignUpPage = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [studentNumber, setStudentNumber] = useState<string>('');
+
     return(
         <div>
             <div>SignUpPage</div>
