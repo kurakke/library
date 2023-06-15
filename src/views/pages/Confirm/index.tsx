ca9da31eaa3be6324fb21c6247ec4c8a442469e6
kurakke <kcanggua@gmail.com>
2023-06-15 17:45:37 +0900

    useAuth導入

--- src/views/pages/Confirm/index.tsx
+++ src/views/pages/Confirm/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useAuth } from "~/features/auth/hooks/useAuth";
 
 export const ConfirmPage = () => {
+    const auth = useAuth();
+    
     return (
         <div>
             <div>
