53e52b3b620fe720a0373f1140789331403f8a27
kurakke <kcanggua@gmail.com>
2023-06-15 17:55:50 +0900

    confirmCode用のform作った

--- src/views/pages/Confirm/index.tsx
+++ src/views/pages/Confirm/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import { useAuth } from "~/features/auth/hooks/useAuth";
 
 export const ConfirmPage = () => {
     const auth = useAuth();
-    
+    const [confirmCode, setConfirmCode] = useState<string>('');
+
+    const handleConfrimCode = (event) => {
+        setConfirmCode(event.target.value);
+    }
+    const handleSubmit = () => {
+        console.log('submit');
+    }
+
     return (
         <div>
             <div>
-                Hello Confirm
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>
+                            confirmCode
+                        </label>
+                        <input type="text" name="confirmCode" value={confirmCode} onChange={handleConfrimCode} />
+                    </div>
+                    <button type="submit">submit</button>
+                </form>
             </div>
         </div>
     )
