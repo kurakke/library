e9d08a6ff8d6d36715fea1486505cfed68c95392
saezuri510 <saezuru510@gmail.com>
2023-06-22 01:21:24 +0900

    feat: Bookのリスト部分を軽く実装しました.

--- src/ui/components/Book/index.tsx
+++ src/ui/components/Book/index.tsx
@@ -1,18 +1,21 @@
-import {BookEntity} from "~/features/book/entities";
+import { BookEntity } from "~/features/book/entities";
 
 export type Props = {
     book: BookEntity;
 };
 
-export const Book = ({book}: Props) => {
+export const Book = ({ book }: Props) => {
     return (
         <div>
-            <div className="w-full mt-12 rounded-3xl aspect-video bg-expressive-red"/>
+            <div className="w-full mt-12 rounded-3xl aspect-video bg-expressive-red" />
             <div className="flex justify-between mt-8">
                 <h3 className="font-black">{book.title}</h3>
                 <div className="lib-tag bg-brand-green text-white">タグ</div>
             </div>
-            <p className="text-xs">{book.title}</p>
+            <div className="flex space-x-[4px]">
+                <div className="text-xs">タイトル: {book.title},</div>
+                <div className="text-xs">著者: {book.owner}</div>
+            </div>
         </div>
     );
 };
