c3856a172e2a629e26892c03e8bb0d3c862c39de
saezuri510 <saezuru510@gmail.com>
2023-06-24 19:07:22 +0900

    feat: bookSearchを実装しました.

--- src/views/pages/Books/index.tsx
+++ src/views/pages/Books/index.tsx
@@ -1,55 +1,61 @@
-import {useState} from "react";
-import {SearchInput} from "~/ui/components/SearchInput";
-import {DefaultLayout} from "~/ui/layouts/Default";
-import {Book} from "~/ui/components/Book";
-import {PAGE_PATH} from "~/features/application/constants/page";
-import {useRouter} from "next/router";
-import {getServerSideProps} from "~/views/pages/Books/beforeRender";
-import {InferGetServerSidePropsType} from "next";
-import {Tab, DisplayStatus} from "~/ui/components/Tab";
-import {createDynamicUrl} from "~/features/application/utils/url";
+import { useState } from "react";
+import { SearchInput } from "~/ui/components/SearchInput";
+import { DefaultLayout } from "~/ui/layouts/Default";
+import { Book } from "~/ui/components/Book";
+import { PAGE_PATH } from "~/features/application/constants/page";
+import { useRouter } from "next/router";
+import { getServerSideProps } from "~/views/pages/Books/beforeRender";
+import { InferGetServerSidePropsType } from "next";
+import { Tab, DisplayStatus } from "~/ui/components/Tab";
+import { createDynamicUrl } from "~/features/application/utils/url";
 import Link from "next/link";
 
-
-export const BooksPage = (
-    {bookList}: InferGetServerSidePropsType<typeof getServerSideProps>
-) => {
-    const router = useRouter()
+export const BooksPage = ({
+    searchResult,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    const router = useRouter();
     const handleSearch = (result: string) => {
         router.push({
             pathname: PAGE_PATH.Books,
             query: {
-                keyword: result
-            }
-        })
-    }
+                keyword: result,
+            },
+        });
+    };
 
-    const [displayStatus, setDisplayStatus] = useState<DisplayStatus>(DisplayStatus.All);
+    const [displayStatus, setDisplayStatus] = useState<DisplayStatus>(
+        DisplayStatus.All
+    );
     const handleChangeTab = (result: DisplayStatus) => {
-        setDisplayStatus(result)
-    }
+        setDisplayStatus(result);
+    };
 
     return (
         <div>
             <DefaultLayout>
                 <div className="px-16 mt-16">
-                    <SearchInput onSearch={handleSearch}/>
+                    <SearchInput onSearch={handleSearch} />
                     <div className="mt-16">
-                        <Tab displayStatus={displayStatus} onChange={handleChangeTab}/>
-                        {
-                            bookList.list.map((book) =>
-                                <Link href={createDynamicUrl(PAGE_PATH.BookOne, {
+                        <Tab
+                            displayStatus={displayStatus}
+                            onChange={handleChangeTab}
+                        />
+                        {searchResult.list.map((book) => (
+                            <Link
+                                href={createDynamicUrl(PAGE_PATH.BookOne, {
                                     bookId: book.id,
-                                })}>
-                                    <a>
-                                        <li key={book.id}><Book book={book}/></li>
-                                    </a>
-                                </Link>
-                            )
-                        }
+                                })}
+                            >
+                                <a>
+                                    <li key={book.id}>
+                                        <Book book={book} />
+                                    </li>
+                                </a>
+                            </Link>
+                        ))}
                     </div>
                 </div>
             </DefaultLayout>
         </div>
-    )
-}
+    );
+};
