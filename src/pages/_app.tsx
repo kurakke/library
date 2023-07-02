eac72980f9c2dda413ee7682a12028dde4c94847
saezuri510 <saezuru510@gmail.com>
2023-07-03 00:41:37 +0900

    feat: titleを追加しました.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import { ProvideAuth } from "~/features/auth/hooks/useAuth";
 import "../styles/index.css";
+import Head from "next/head";
 
 // HACK: AppPropsを使用するとエラーが発生していたため、独自で型定義をしました。
 function MyApp({
@@ -12,6 +13,9 @@ function MyApp({
 }) {
     return (
         <>
+            <Head>
+                <title>コン研 - 図書館</title>
+            </Head>
             <ProvideAuth>
                 <Component {...pageProps} />
                 <Toaster position="bottom-center" />
