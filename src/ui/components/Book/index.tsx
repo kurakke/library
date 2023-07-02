c04bf0faf52e863f17c165a3b7f124e7ead8417b
saezuri510 <saezuru510@gmail.com>
2023-07-02 23:44:07 +0900

    feat: ランダムを削除しました.

--- src/ui/components/Book/index.tsx
+++ src/ui/components/Book/index.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 import { PAGE_PATH } from "~/features/application/constants/page";
 import { createDynamicUrl } from "~/features/application/utils/url";
 import { BookEntity } from "~/features/book/entities";
-import Book0Img from "~/assets/pngs/book0.png";
-import Book1Img from "~/assets/pngs/book1.png";
-import Book2Img from "~/assets/pngs/book2.png";
 
 export type Props = {
     book: BookEntity;
@@ -33,23 +30,9 @@ export const Book = ({ book, isWithoutLink = false }: Props) => {
 };
 
 const BookContent = ({ book }: { book: BookEntity }) => {
-    // TODO: これは仮の画像を取得するための仮の処理なので必要に応じて削除してください.
-    const getImage = () => {
-        const random = Math.floor(Math.random() * 3);
-
-        switch (random) {
-            case 0:
-                return <Image src={Book0Img} width={300} height={223} />;
-            case 1:
-                return <Image src={Book1Img} width={300} height={223} />;
-            default:
-                return <Image src={Book2Img} width={300} height={223} />;
-        }
-    };
-
     return (
         <div>
-            {getImage()}
+            <Image src="/webp/book.webp" width={300} height={223} />
             <div className="flex justify-between mt-8">
                 <h3 className="font-black">{book.title}</h3>
                 <div className="lib-tag bg-brand-green text-white">タグ</div>
